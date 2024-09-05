bcd36e6dd7e0243cc4a160d4bcffef5a845ea783
Moonn03 <samantha_sanchez@anahuacmayab.edu.mx>
2024-09-04 21:55:44 -0600

    aaa

---
+++ Assets/Scripts/PointUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PointUI : MonoBehaviour
+{
+    //VARIABLES
+    public TMP_Text ScoreText;
+
+    void Update()
+    {
+        ScoreText.text = PointManager.instance.GetScore().ToString();
+    }
+    public void OnClick_AddPoints() 
+    {
+        //DESACTIVAR EL COLLIDER.
+        //ACTIVAR SFX.
+        //DESACTIVAR RENDER MESH.
+        PointManager.instance.AddPoints(10);
+    
+    }
+    public void Onclick_SubtractPoint() 
+    {
+        PointManager.instance.RemovePoints(5);    
+    
+    
+    }
+}
